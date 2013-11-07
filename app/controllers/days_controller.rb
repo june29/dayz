028b38588677b6300d101a7eafbb45c192d7c394
june29 <june29.jp@gmail.com>
2013-11-07 19:20:18 +0900

    Refactor

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,13 +1,13 @@
 class DaysController < ApplicationController
-  DAYS_PER_PAGE = 10
+  DAYS_PER_PAGE = 14
 
   def index
     @days_count = Day.count
 
-    today        = Date.today
-    ten_days_ago = today - (DAYS_PER_PAGE - 1)
+    start_on = Date.today
+    end_on   = start_on - (DAYS_PER_PAGE - 1)
 
-    @days = (ten_days_ago..today).to_a.reverse.map { |date|
+    @days = (end_on..start_on).to_a.reverse.map { |date|
       Hashie::Mash.new({ date: date, day: Day.find_by_date(date) })
     }
   end
