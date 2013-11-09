6963d0c9829d924c73a0f897e46c7848e91c4457
june29 <june29.jp@gmail.com>
2013-11-09 16:24:03 +0900

    Implement pager

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -2,13 +2,30 @@ class DaysController < ApplicationController
   DAYS_PER_PAGE = 14
 
   def index
-    @day_count = Day.count
+    before = params[:before]
 
-    start_on = Date.today
+    today = Date.today
+
+    start_on = before.present? ? Date.parse(before) : today
     end_on   = start_on - (DAYS_PER_PAGE - 1)
+
+    @newer = (start_on >= today - 1) ? nil : [start_on + DAYS_PER_PAGE, today].min
+    @older = end_on - 1
+
+    @day_count = Day.count
 
     @days = (end_on..start_on).to_a.reverse.map { |date|
       Hashie::Mash.new({ date: date, day: Day.find_by_date(date) })
+    }
+  end
+
+  def search
+    @query = params[:q]
+    result = Day.search(note_cont: @query).result
+    @result_count = result.count
+
+    @days = result.map { |d|
+      Hashie::Mash.new({ date: d.date, day: d })
     }
   end
 
@@ -38,15 +55,5 @@ class DaysController < ApplicationController
     else
       head 500
     end
-  end
-
-  def search
-    @query = params[:q]
-    result = Day.search(note_cont: @query).result
-    @result_count = result.count
-
-    @days = result.map { |d|
-      Hashie::Mash.new({ date: d.date, day: d })
-    }
   end
 end
